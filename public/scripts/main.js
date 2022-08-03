89dca618065bbc77d28dc7171b274b8355f5b72a
Alptekin I <alptekin@192.168.1.6>
2022-08-03 04:23:23 +0300

    forms style modified. dom manipulation added.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,6 +8,23 @@ form.addEventListener("submit", event => {
  */
 
 
+const signupButton = document.getElementById('button-for-signup');
+
+const signupForm = document.getElementById('signup-form');
+
+const signinForm = document.getElementById('signin-form')
+
+const showSignupButton = document.getElementById("button-for-signup");
+
+showSignupButton.addEventListener('click', showSignup);
+
+function showSignup() {
+    console.log("showSignup function triggered");
+    signupForm.style.display = "block";
+    signinForm.replaceWith(signupForm);
+}
+
+
 function pressSignup(event) {
     console.log("button pressed")
 }
