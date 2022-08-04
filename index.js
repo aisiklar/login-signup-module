43852a4d577e3c5660dcb5f0445cd73e5f70da47
aisiklar <maisiklar@gmail.com>
2022-08-04 13:16:26 +0300

    working on db registry input

--- index.js
+++ index.js
@@ -18,15 +18,30 @@ app.use('/', express.static('public'));
 // create a database and load automatically
 let db = new Datastore({ filename: './database/accounts.db', autoload: true });
 
-app.post('/', (req, res, next) => {
+app.post('/signin', (req, res, next) => {
    console.log(req.body);
-   let register = req.body;
-   let username = register.email;
-   let name = register.name;
-   let password = register.password;
-   console.log(`name, username, password: ${name}, ${username}, ${password}`);
+   let input = req.body;
+   let username = input.username;
+   let password = input.password;
+//   db.insert(input, function (err, newDoc) { 
+   res.send(".....................")
+   console.log(`username, password: ${username}, ${password}`);
+});
+
+app.post('/signup', (req, res, next) => {
+   console.log(req.body);
+   let username = req.body.email;
+   let name = req.body.name;
+   let password = req.body.pword;
+   let register = [username =  {
+       'some name': name, 
+       'some password': password
+ }];
+
+
    db.insert(register, function (err, newDoc) { 
-    console.log("register is saved to db...")
+     console.log("register is saved to db...")
    });
    res.send("you are registered!")
+   console.log(`name, username, password: ${name}, ${username}, ${pword}`);
 });
