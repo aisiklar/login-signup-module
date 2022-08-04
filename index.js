e518eaf8f01dac2d7457f02f7843e26edd947396
aisiklar <maisiklar@gmail.com>
2022-08-04 17:37:13 +0300

    worked on writing to db and indexing. bugs exist

--- index.js
+++ index.js
@@ -18,7 +18,26 @@ app.use('/', express.static('public'));
 // create a database and load automatically
 let db = new Datastore({ filename: './database/accounts.db', autoload: true });
 
-let id = 0;
+let numberOfAccounts = 0;
+
+db.count({}, (err, count) => {
+   console.log(`count: ${count}`);
+   numberOfAccounts = count;
+   console.log(`numberOfAccounts: ${numberOfAccounts}`);
+})
+
+db.find({ _id: '0' }, function (err, docs) {
+  // If no document is found, docs is equal to []
+  if (docs === []) {
+   id = 0;
+  } 
+  else {
+   id = numberOfAccounts;
+  }
+});
+
+
+//console.log(`id: ${id}`);
 
 app.post('/signin', (req, res, next) => {
    console.log(req.body);
@@ -30,8 +49,10 @@ app.post('/signin', (req, res, next) => {
    console.log(`username, password: ${username}, ${password}`);
 });
 
-app.post('/', (req, res, next) => {
+app.post('/signup', (req, res, next) => {
+   console.log('received post request');
    console.log(req.body);
+   console.log(`id at the start of post service: ${id}`);
    let username = req.body.email;
    let name = req.body.name;
    let password = req.body.pword;
@@ -39,15 +60,19 @@ app.post('/', (req, res, next) => {
       '_id': id,
        'name': name, 
        'password': password
- };
+   };
    for (elements in register) {
       console.log(elements, register[elements]);
    }  
 
-   db.insert(register, function (err, newDoc) { 
-     console.log("register is saved to db...")
+   db.insert(register, (err, newDoc) => { 
+     console.log("register is saved to db...");
+     console.log(`newDoc: ${newDoc}`);
    });
+
    res.send("you are registered!")
    console.log(`name, username, password: ${name}, ${username}, ${password}`);
+   console.log(`id before incrementing: ${id}`);
    id++;   
+   console.log(`id after incrementing: ${id}`);
 });
