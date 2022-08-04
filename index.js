d594067d57326e870ea4deef1e74bafb4d625896
aisiklar <maisiklar@gmail.com>
2022-08-04 14:25:58 +0300

    worked on db format

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use('/', express.static('public'));
 // create a database and load automatically
 let db = new Datastore({ filename: './database/accounts.db', autoload: true });
 
+let id = 0;
+
 app.post('/signin', (req, res, next) => {
    console.log(req.body);
    let input = req.body;
@@ -28,20 +30,24 @@ app.post('/signin', (req, res, next) => {
    console.log(`username, password: ${username}, ${password}`);
 });
 
-app.post('/signup', (req, res, next) => {
+app.post('/', (req, res, next) => {
    console.log(req.body);
    let username = req.body.email;
    let name = req.body.name;
    let password = req.body.pword;
-   let register = [username =  {
-       'some name': name, 
-       'some password': password
- }];
-
+   let register = {
+      '_id': id,
+       'name': name, 
+       'password': password
+ };
+   for (elements in register) {
+      console.log(elements, register[elements]);
+   }  
 
    db.insert(register, function (err, newDoc) { 
      console.log("register is saved to db...")
    });
    res.send("you are registered!")
-   console.log(`name, username, password: ${name}, ${username}, ${pword}`);
+   console.log(`name, username, password: ${name}, ${username}, ${password}`);
+   id++;   
 });
